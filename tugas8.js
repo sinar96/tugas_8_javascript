1eb049af4ba638897b734e5df53bdca86a369b11
sinar96 <sinar96>
2020-01-17 20:16:06 +0800

    Learn how to use object in JS

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function panggil() {
+    var saya = {
+        sinar : 1,
+        mustika : 2,
+        ruslan : 3
+    }
+    console.log(saya);
+    saya.mustika = 5;
+    console.log(saya);
+    // console.log(saya.mustika);
+    
+}
+panggil();
